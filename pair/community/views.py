690e55bf26290e7cdcd3029e7053570826679ae0
hae-on <solbi2004@naver.com>
2022-09-30 13:03:33 +0900

    구현 완료

--- pair/community/views.py
+++ pair/community/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Review
 
 # Create your views here.
+
+
+def index(request):
+    reviews = Review.objects.all()
+    context = {
+        "reviews": reviews,
+    }
+    return render(request, "community/index.html", context)
+
+
+def new(request):
+    return render(request, "community/new.html")
+
+
+def detail(request, pk):
+    review = Review.objects.get(pk=pk)
+
+    context = {
+        "review": review,
+    }
+    return render(request, "community/detail.html", context)
+
+
+def create(request):
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    Review.objects.create(title=title, content=content)
+    return redirect("community:index")
+
+
+def edit(request, pk):
+    review = Review.objects.get(pk=pk)
+    context = {
+        "review": review,
+    }
+    return render(request, "community/edit.html", context)
+
+
+def update(request, pk):
+    review = Review.objects.get(pk=pk)
+
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    review.title = title
+    review.content = content
+
+    review.save()
+
+    return redirect("community:detail", review.pk)
+
+
+def delete(request, pk):
+    review = Review.objects.get(pk=pk)
+    review.delete()
+    return redirect("community:index")
